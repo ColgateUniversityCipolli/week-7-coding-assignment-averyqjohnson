fe634df2fb158d6f48f750a6a0b722cf661661d8
averyqjohnson <aqjohnson@colgate.edu>
2025-03-06 13:02:17 -0500

    Code Complete

--- HW7Code.R
+++ HW7Code.R
@@ -4,6 +4,39 @@
 ###############################################################################
 
 # Step 1: Write a pois.prob() function
-
+pois.prob <- function(x, lambda, type){
+  if (type == "="){
+    return (dpois(x, lambda))
+  } else if (type== "!=") {
+    return (1 - dpois(x, lambad))
+  } else if (type== "<"){
+    return(ppois(x-1, lambda))
+  } else if (type == "<=") {
+    return(ppois(x, lambda))
+  } else if (type == ">") {
+    return(1 - ppois(x, lambda))
+  } else if (type == ">="){
+    return(1 - ppois(x-1, lambda))
+  }
+}
 
 # Step 2: Write a beta.prob() function
+beta.prob <- function(x, alpha, beta, type){
+  if (type == "="){
+    return(0)
+  } else if (type== "!=") {
+    return(1)
+  } else if (type== "<"){
+    return(pbeta(x, alpha, beta))
+  } else if (type == "<=") {
+    return(pbeta(x, alpha, beta))
+  } else if (type == ">") {
+    return(1 - pbeta(x, alpha, beta))
+  } else if (type == ">="){
+    return(1 - pbeta(x, alpha, beta))
+  }
+}
+
+# example
+pois.prob(x=0, lambda=2, type="=")
+beta.prob(x=0.4, alpha=2, beta=5, ">=")
